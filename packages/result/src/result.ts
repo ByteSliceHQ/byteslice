ebdb2e2889b5d6e52d3ca995da5a75d637ee2687
J. Eric Bukowski <eric@byteslice.co>
2024-09-01 11:04:49 -0600

    feat(result): ensure error is provided to callback

--- packages/result/src/result.ts
+++ packages/result/src/result.ts
@@ -16,13 +16,17 @@ type FailureOption = FailureCase | Error
 
 export type Result<S, F extends FailureOption> = Success<S> | Failure<F>
 
+function ensureError(ex: unknown): Error {
+  return ex instanceof Error ? ex : Error('Something went wrong')
+}
+
 export async function withResult<S, F extends FailureOption>(
   operation: S | Promise<S>,
-  onException: (ex: unknown) => F,
+  onError: (error: Error) => F,
 ): Promise<Result<S, F>> {
   try {
     return { result: await operation }
   } catch (ex) {
-    return { failure: onException(ex) }
+    return { failure: onError(ensureError(ex)) }
   }
 }
