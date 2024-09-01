29f730d874366a26a47b1dddf5a378025824e490
J. Eric Bukowski <eric@byteslice.co>
2024-09-01 10:33:56 -0600

    feat(result): add result wrapper

--- packages/result/src/result.ts
+++ packages/result/src/result.ts
@@ -14,3 +14,14 @@ type FailureCase = {
 type FailureOption = FailureCase | Error
 
 export type Result<S, F extends FailureOption> = Success<S> | Failure<F>
+
+export async function withResult<S, F extends FailureOption>(
+  operation: S | Promise<S>,
+  onException: (ex: unknown) => F,
+): Promise<Result<S, F>> {
+  try {
+    return { result: await operation }
+  } catch (ex) {
+    return { failure: onException(ex) }
+  }
+}
