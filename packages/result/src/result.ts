8e48625e3a62b8745efdbc645f4efef882cf057a
J. Eric Bukowski <eric@byteslice.co>
2024-09-01 11:12:10 -0600

    feat(result): permit error fallback

--- packages/result/src/result.ts
+++ packages/result/src/result.ts
@@ -16,17 +16,21 @@ type FailureOption = FailureCase | Error
 
 export type Result<S, F extends FailureOption> = Success<S> | Failure<F>
 
-function ensureError(ex: unknown): Error {
-  return ex instanceof Error ? ex : Error('Something went wrong')
+function ensureError(
+  ex: unknown,
+  fallback: Error = new Error('Something went wrong'),
+): Error {
+  return ex instanceof Error ? ex : fallback
 }
 
 export async function withResult<S, F extends FailureOption>(
   operation: S | Promise<S>,
   onError: (error: Error) => F,
+  fallback?: Error,
 ): Promise<Result<S, F>> {
   try {
     return { result: await operation }
   } catch (ex) {
-    return { failure: onError(ensureError(ex)) }
+    return { failure: onError(ensureError(ex, fallback)) }
   }
 }
