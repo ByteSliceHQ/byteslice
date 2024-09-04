aa354047720b81321e0f4814ef54e111d68c03ed
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-09-04 09:19:04 -0600

    feat(result): create package for result pattern (#1)

---
+++ packages/result/src/result.ts
@@ -0,0 +1,32 @@
+type Success<T> = {
+  failure?: never
+  data: T
+}
+
+type Failure<T> = {
+  failure: T
+}
+
+type FailureCase = {
+  type: string
+}
+
+type FailureOption = FailureCase | Error
+
+export type Result<S, F extends FailureOption = Error> = Success<S> | Failure<F>
+
+function ensureError(ex: unknown): Error {
+  return ex instanceof Error ? ex : new Error('Something went wrong')
+}
+
+// Wraps operation with structured result (success and failure states).
+export async function withResult<S, F extends FailureOption = Error>(
+  operation: S | Promise<S>,
+  onError: (error: Error) => F,
+): Promise<Result<S, F>> {
+  try {
+    return { data: await operation }
+  } catch (ex) {
+    return { failure: onError(ensureError(ex)) }
+  }
+}
