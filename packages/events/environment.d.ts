9ff621cdc49a4df437610d017a8656c2a014c4cd
CJ Brewer <brewercalvinj@gmail.com>
2025-06-13 12:47:05 -0600

    chore(events): server env

--- packages/events/environment.d.ts
+++ packages/events/environment.d.ts
@@ -1,8 +1,9 @@
 namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_SUPABASE_URL: string
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: string
-    NEXT_PUBLIC_BYTESLICE_EVENTS_CLIENT: string
-    RESEND_API_KEY: string
+    BYTESLICE_SUPABASE_URL: string
+    BYTESLICE_SUPABASE_ANON_KEY: string
+    BYTESLICE_EVENTS_CLIENT: string
+    BYTESLICE_RESEND_API_KEY: string
+    BYTESLICE_LOG_LEVEL: 'info' | 'debug' | 'none'
   }
 }
